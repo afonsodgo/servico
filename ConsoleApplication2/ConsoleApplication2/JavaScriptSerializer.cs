41b2e4bf1441e337a816d8df4b53b25df9621716
Afonso Dal Gallo de Oliveira <afonso@cit>
2016-03-02 16:54:17 -0300

    criando projeto

---
+++ ConsoleApplication2/ConsoleApplication2/JavaScriptSerializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplication2
+{
+    internal class JavaScriptSerializer
+    {
+        internal Saude DeserializeObject(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Uri uri = new Uri(@"http://api.ima.sp.gov.br/v1/saude?offset=0&limit=10");
+
+            WebRequest webRequest = WebRequest.Create(uri);
+            webRequest.Headers.Add("CLIENT_ID", "yYK6BHZC8FW7");
+            webRequest.Headers.Add("ACCESS-TOKEN", "yYK6BHZC8FW7");
+
+            WebResponse response = webRequest.GetResponse();
+
+            StreamReader streamReader = new StreamReader(response.GetResponseStream());
+
+            String responseData = streamReader.ReadToEnd();
+
+            var resultado = JsonConvert.DeserializeObject<List<Saude>>(responseData);
+
+            foreach (var item in resultado) {
+
+                Console.WriteLine(String.Format("Id: {0}, Local de Atendimento {1} \n", item.ID, item.localAtendimento));
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Saude
+    {
+
+        public string ID { get; set; }
+        public string distritoVinculo { get; set; }
+        public string municipio { get; set; }
+        public string uf { get; set; }
+        public string localAtendimento { get; set; }
+        public string distritoAtendimento { get; set; }
+        public string dataAtendimento { get; set; }
+        public string codigoTipoAtendimento { get; set; }
+        public string descricaoTipoAtendimento { get; set; }
+        public string descricaoGrupoAtendimento { get; set; }
+        public string codigoGrupoAtendimentoSUS { get; set; }
+        public string ocupacaoProfissional { get; set; }
+        public string descricaoTipoVinculoSMS { get; set; }
+        public string codigoProcedimentoSUS { get; set; }
+        public string descricaoProcedimento { get; set; }
+        public string codigoAtividadeProfissionalSUS { get; set; }
+        public string descricaoAtividadeProfissional { get; set; }
+        public string sexo { get; set; }
+        public string idade { get; set; }
+        public string hora { get; set; }
+        public string periodo { get; set; }
+        public string dataNascimento { get; set; }
+        public string quantidadeRealizada { get; set; }
+
+    }
+}
